a984ff4cc9959c59d066a3053b8f8dc955e7e128
YSshi98 <shiyanshuai98@163.com>
2023-06-08 01:53:59 +0800

    Add comment to code

--- PartA.py
+++ PartA.py
@@ -6,36 +6,59 @@ from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def load_data(training_path, testing_path):
+    # Load training data from the specified path
     training_data = pd.read_csv(training_path)
+
+    # Load testing data from the specified path
     testing_data = pd.read_csv(testing_path)
 
+    # Extract the features (inputs) from the training data
     X_train = training_data.iloc[:, :-1]
+
+    # Extract the labels (outputs) from the training data
     y_train = training_data.iloc[:, -1]
+
+    # Assign the testing data as the input for testing
     X_test = testing_data
 
+    # Return the training features, training labels, and testing features
     return X_train, y_train, X_test
 
 def create_model(X_train, y_train):
+    # Create a RandomForestClassifier model with a random_state of 0
     model = RandomForestClassifier(random_state=0)
+
+    # Perform cross-validation using the training data
     scores = cross_val_score(model, X_train, y_train, cv=5, scoring='f1_macro')
 
-    # Display cross-validation scores as a table
+    # Display the cross-validation scores as a table
     scores_table = pd.DataFrame(scores, columns=['Cross-Validation Scores'])
     scores_table.index.name = 'Iteration'
     print(scores_table)
 
+    # Fit the model on the entire training data
     model.fit(X_train, y_train)
 
+    # Return the trained model
     return model
 
 def save_results(testing_data, predicted_labels, output_path):
+    # Create a DataFrame with the testing data
     testing_results = pd.DataFrame(testing_data)
+
+    # Add a column 'marker' containing the predicted labels
     testing_results['marker'] = predicted_labels
+
+    # Save the results to a CSV file at the specified output path
     testing_results.to_csv(output_path, index=False)
 
 def plot_confusion_matrix(y_true, y_pred):
+    # Compute the confusion matrix
     cm = confusion_matrix(y_true, y_pred)
+
+    # Plot the confusion matrix as a heatmap with annotations
     sns.heatmap(cm, annot=True, fmt="d")
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted')
@@ -43,20 +66,37 @@ def plot_confusion_matrix(y_true, y_pred):
     plt.show()
 
 def plot_feature_importances(model):
+    # Calculate feature importances
     importances = model.feature_importances_
+    # Sort indices in descending order
     indices = np.argsort(importances)[::-1]
+
+    # Create a new figure for the plot
     plt.figure()
+    # Set the title of the plot
     plt.title("Feature importances")
+    # Create a bar plot with the sorted importances
     plt.bar(range(X_train.shape[1]), importances[indices], color="r", align="center")
+    # Set the x-axis labels to the sorted indices
     plt.xticks(range(X_train.shape[1]), indices)
+    # Set the x-axis limits
     plt.xlim([-1, X_train.shape[1]])
+    # Show the plot
     plt.show()
 
+# Load the training and testing data
 X_train, y_train, X_test = load_data("TrainingDataBinary.csv", "TestingDataBinary.csv")
+# Create a machine learning model
 model = create_model(X_train, y_train)
+# Predict labels for the testing data
 predicted_labels = model.predict(X_test)
+# Save the predicted labels to a file
 save_results(X_test, predicted_labels, "TestingResultsBinary.csv")
+# Calculate the F1 score on the training data
 f1 = f1_score(y_train, model.predict(X_train))
+# Print the F1 score
 print("\nF1 Score:", f1)
+# Plot the confusion matrix for the training data
 plot_confusion_matrix(y_train, model.predict(X_train))
+# Plot the feature importances of the model
 plot_feature_importances(model)
--- PartB.py
+++ PartB.py
@@ -8,8 +8,10 @@ from sklearn.metrics import confusion_matrix, f1_score, classification_report
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Function to load data from CSV files
 def load_data(training_file, testing_file):
     try:
+        # Read the training and testing data from CSV files
         training_data = pd.read_csv(training_file)
         testing_data = pd.read_csv(testing_file)
     except ValueError as e:
@@ -23,49 +25,62 @@ def load_data(training_file, testing_file):
     testing_data = testing_data.dropna()
     return training_data, testing_data
 
+# Function to extract features and target variable from training data
 def extract_features(training_data):
-    X = training_data.iloc[:, :-1]
-    y = training_data.iloc[:, -1]
+    X = training_data.iloc[:, :-1]  # Extract all columns except the last one as features
+    y = training_data.iloc[:, -1]   # Extract the last column as the target variable
     return X, y
 
+# Function to split data into training and validation sets
 def split_data(X, y):
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_val, y_train, y_val
 
+# Function to normalize the data
 def normalize_data(X_train, X_val, X_test):
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_val = scaler.transform(X_val)
-    X_test = scaler.transform(X_test)
+    X_train = scaler.fit_transform(X_train)  # Normalize training data
+    X_val = scaler.transform(X_val)          # Normalize validation data
+    X_test = scaler.transform(X_test)        # Normalize testing data
     return X_train, X_val, X_test
 
+from imblearn.over_sampling import SMOTE
+
+# Function to balance the data using SMOTE
 def balance_data(X_train, y_train):
     sm = SMOTE(random_state=42)
     X_train_res, y_train_res = sm.fit_resample(X_train, y_train)
     return X_train_res, y_train_res
 
+from sklearn.ensemble import RandomForestClassifier
+
+# Function to train the random forest classifier model
 def train_model(X_train, y_train):
     model = RandomForestClassifier(random_state=0)
     model.fit(X_train, y_train)
     return model
 
+# Function to make predictions using the trained model
 def make_predictions(X_test, model):
     predicted_labels = model.predict(X_test)
     return predicted_labels
 
+# Function to save the predictions to a CSV file
 def save_predictions(testing_data, predicted_labels, output_file):
     testing_results = pd.DataFrame(testing_data)
     testing_results['label'] = predicted_labels
     testing_results.to_csv(output_file, index=False)
     return testing_results
-
 def evaluate_model(y_val, y_pred):
+    # Calculate the F1 score and print it
     f1 = f1_score(y_val, y_pred, average='macro')
     print("F1 Score:", f1)
+
+    # Print the classification report
     print("Classification Report:")
     print(classification_report(y_val, y_pred))
 
-    # Confusion Matrix
+    # Generate and display the confusion matrix
     cm = confusion_matrix(y_val, y_pred)
     sns.heatmap(cm, annot=True, fmt="d")
     plt.title("Confusion Matrix")
@@ -75,9 +90,12 @@ def evaluate_model(y_val, y_pred):
 
     # Feature Importance
     if isinstance(model, RandomForestClassifier):
+        # Retrieve feature importances from the model
         importances = model.feature_importances_
+        # Sort feature importances in descending order
         indices = np.argsort(importances)[::-1]
 
+        # Plot the feature importances
         plt.figure()
         plt.title("Feature importances")
         plt.bar(range(X_train.shape[1]), importances[indices], color="r", align="center")
@@ -86,13 +104,32 @@ def evaluate_model(y_val, y_pred):
         plt.show()
 
 if __name__ == "__main__":
+    # Load training and testing data
     training_data, testing_data = load_data("TrainingDataMulti.csv", "TestingDataMulti.csv")
+
+    # Extract features from the training data
     X, y = extract_features(training_data)
+
+    # Split the data into training and validation sets
     X_train, X_val, y_train, y_val = split_data(X, y)
+
+    # Normalize the data
     X_train, X_val, testing_data = normalize_data(X_train, X_val, testing_data)
+
+    # Balance the training data
     X_train, y_train = balance_data(X_train, y_train)
+
+    # Train the model
     model = train_model(X_train, y_train)
+
+    # Make predictions on the validation set
     y_pred = make_predictions(X_val, model)
+
+    # Evaluate the model on the validation set
     evaluate_model(y_val, y_pred)
+
+    # Make predictions on the testing data
     predicted_labels = make_predictions(testing_data, model)
+
+    # Save the predicted labels to a file
     save_predictions(testing_data, predicted_labels, "TestingResultsMulti.csv")
